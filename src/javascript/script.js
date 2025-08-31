afd55dfbb083c36e990ff60bf8460de44f38360b
Samuel <lemuays@gmail.com>
2025-08-31 20:13:35 -0300

    Finalizando projeto

---
+++ src/javascript/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('.edit-button').on('click', function () {
+        var $task = $(this).closest('.task');
+        $task.find('.task-description').addClass('hidden');
+        $task.find('.edit-button, .delete-button').addClass('hidden');
+        $task.find('.progress').addClass('hidden');
+        $task.find('.edit-task').removeClass('hidden');
+    });
+        $('.progress').on('click', function () {
+            if ($(this).is(':checked')) {
+                $(this).addClass('done');
+            } else {
+                $(this).removeClass('done');
+            }
+        });
+
+        $('.progress').on('change', function () {
+            const id = $(this).data('task-id');
+            const completed = $(this).is(':checked') ? 'true' : 'false';
+            $.ajax({
+                url: '../../actions/update-progress.php',
+                method: 'POST',
+                data: {id: id, completed: completed},
+                dataType: 'json',
+                success: function (response) {
+                    if (response.success){
+
+                    } else {
+                        alert('Erro ao atualizar o status da tarefa.');
+                    }
+                },
+                error: function() {
+                    alert('Erro na requisição. Tente novamente.');
+                }
+                
+            })
+        })
+});
